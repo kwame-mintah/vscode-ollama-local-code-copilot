d8b0204988c0b6a4dd8877ff8568a4e1cb405b57
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-02-03 21:43:57 +0000

    refactor(docker-entrypoint): include comment for single vs double quotes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 # See Ollama documentation for pull model(s): https://github.com/ollama/ollama/blob/v0.1.23/docs/api.md
+# Single quotes won't interpolate anything, but double quotes will.
 for i in $(echo $OLLAMA_MODELS | sed "s/,/ /g")
 do
     curl http://ollama:11434/api/pull -d "{\"name\": \"$i\"}"

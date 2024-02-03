3dafd25ee04a8e84e619f52ff355bd0a647095ba
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-02-03 21:22:04 +0000

    feat(docker-compose): allow for multiple ollama models to be pulled
    `docker-entrypoint` updated to allow more models to be pulled using environment variable `OLLAMA_MODELS` as a comma seperated list. provided `.env.example` to include ollama docker tag to be pulled, defaults to `latest`.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
 
-# See Ollama documentation for pull model(s): https://github.com/ollama/ollama/blob/main/docs/api.md#pull-a-model
-curl http://ollama:11434/api/pull -d '{"name": "codellama"}'
+# See Ollama documentation for pull model(s): https://github.com/ollama/ollama/blob/v0.1.23/docs/api.md
+for i in $(echo $OLLAMA_MODELS | sed "s/,/ /g")
+do
+    curl http://ollama:11434/api/pull -d "{\"name\": \"$i\"}"
+done

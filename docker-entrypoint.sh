525e84043e9b48b3673cc23d5218776ea06eecc6
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2025-02-16 11:16:58 +0000

    build(docker-compose): pull recommended llama code stable model
    use `stable-code:3b-code-q4_0` as the initial model pulled on start up

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-# See Ollama documentation for pull model(s): https://github.com/ollama/ollama/blob/v0.1.23/docs/api.md
+# See Ollama documentation for pull model(s): https://github.com/ollama/ollama/blob/v0.5.11/docs/api.md#pull-a-model
 # Single quotes won't interpolate anything, but double quotes will.
 for i in $(echo $OLLAMA_MODELS | sed "s/,/ /g")
 do
